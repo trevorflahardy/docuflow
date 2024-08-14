1afdfcf860896aba594361dd11ea3edb40f9c9f2
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-08-14 10:00:30 -0400

    Update proj description

--- src/docuflow.config.ts
+++ src/docuflow.config.ts
@@ -1,5 +1,11 @@
 import { ConfigOptions } from "./docuflow/interfaces";
 
+
+const projectDescription = `
+A lightweight documentation generator that allows users to create documentation from Markdown (MD) and Markdown + JSX (MDX). It's designed to be elegant whilst providing
+a great deal of customization for the user.
+`
+
 /**
  * The configuration options for the docuflow project. In runtime, the modules
  * will be generated based on the docs directory and placed in a modules.json file.
@@ -8,7 +14,7 @@ const options: ConfigOptions = {
     docsDir: "./docs",
     outDir: "./dist",
     projectName: "Docuflow",
-    projectDescription: "A documentation generator for your project.",
+    projectDescription: projectDescription,
     theme: {
         accent: {
             light: "#FF1E00",
