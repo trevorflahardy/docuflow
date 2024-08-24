47879232abe932c8b9dedecbd8e70e8de0093635
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-08-24 17:12:06 -0400

    chore: Update npm dependencies and configure remark-gfm plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import mdx from '@mdx-js/rollup'
+import remarkGfm from 'remark-gfm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    { enforce: 'pre', ...mdx({ jsxImportSource: 'react' }) },
+    {
+      enforce: 'pre', ...mdx({
+        jsxImportSource: 'react', remarkPlugins: [
+          remarkGfm
+        ]
+      })
+    },
     react()
   ],
   publicDir: 'src/public'
